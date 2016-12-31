efb78d9d21a20acc61054d0d8cf01ea029017069
CaitlinJD <caitlin_dyk@hotmail.com>
2016-12-31 17:10:03 -0500

    Journal archive and single mobile styling
    Also includes sidebar mobile styling

--- wp-content/themes/redstarter-master/js/localScript.js
+++ wp-content/themes/redstarter-master/js/localScript.js
@@ -59,22 +59,20 @@ jQuery(document).ready(function($){
     })
 
     // Adventure image height
+        function resizeImage () {
+            var image = $('.post-two');
+            var width = image.width();
+            var height = width / 2;
 
-    function resizeImage () {
-        var image = $('.post-two');
-        var width = image.width();
-        var height = width / 2;
+            image.css('height', height);
+        }
 
-        image.css('height', height);
-    }
-
-    // Set initial image size
-    resizeImage();
-
-    // Change image size as screen size changes
-    $( window ).resize(function() {
+        // Set initial image size
         resizeImage();
-    });
 
+        // Change image size as screen size changes
+        $( window ).resize(function() {
+            resizeImage();
+        });
 
 })
